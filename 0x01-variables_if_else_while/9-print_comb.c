0390fd2a811c381c0cdb0a5df2eebc38af2cbb4e
Adeoluwa2022 <ojoadeoluwasam@gmail.com>
2022-09-12 21:32:30 -0700

    fixed

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,8 +10,8 @@ int main(void)
 	int c;
 
 	for (c = 48; c <= 57; c++)
-	{				
-		putchar(c);	      						  if (c != 57)
+	{
+		putchar(c);	    						  if (c != 57)
 		{
 			putchar(',');
 			putchar(' ');
