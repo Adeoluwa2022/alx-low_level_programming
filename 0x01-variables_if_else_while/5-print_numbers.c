c015998d4823afb7d3dbbfa99cadac44fab784a3
Adeoluwa2022 <ojoadeoluwasam@gmail.com>
2022-09-12 21:28:53 -0700

    fixed

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,11 +8,11 @@
 */
 int main(void)
 {
-char ch;
-for (ch = 'a' ; ch <= 'z' ; ch++)
+int i;
+for (i = 0; i < 10; i++)
 {
-	putchar(ch);
+	printf("%i", i);
 }
-putchar('\n');
+printf("\n");
 return (0);
 }
