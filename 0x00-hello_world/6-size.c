4506d2659d523d339288912ecbe1b8f70786300c
Adeoluwa2022 <ojoadeoluwasam@gmail.com>
2022-09-12 18:54:15 -0700

    Task 4-6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a text
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
