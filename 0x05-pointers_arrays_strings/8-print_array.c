c7cca141e5b6450a173eadf911518b11b1864045
Adeoluwa2022 <ojoadeoluwasam@gmail.com>
2022-09-20 21:18:06 -0700

    strcpy

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,7 +15,7 @@ void print_array(int *a, int n)
 	for (inc = 0; inc < n ; inc++)
 	{
 		if (inc != n - 1)
-			printf("%d, ", a[inc])
+			printf("%d, ", a[inc]);
 		else
 			printf("%d", a[inc]);
 	}
